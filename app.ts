3623f8a330c27a29da4a25f8180125c854e2fc46
Baby <89675173+Swag666baby@users.noreply.github.com>
2023-06-18 20:56:56 -0300

    Update app.ts

--- app.ts
+++ app.ts
@@ -17,5 +17,6 @@ app.get('/:value', (request, response) =>{
     else if(value == "yuri") randomImages("media/yuri", response)
     else if(value == "yaoi") randomImages("media/yaoi", response)
     else if(value == "trap") randomImages("media/trap", response)
+    else response.send({"response":"see the routes in : https://github.com/Swag666baby/nsfw-api"})
 });
 app.listen(3000, () => console.log('server running on port 3000'));
