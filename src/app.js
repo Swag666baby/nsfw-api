c14410da405e63d4bede9944a2adcba702e183fc
Baby <89675173+Swag666baby@users.noreply.github.com>
2023-07-06 22:44:33 -0300

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
 const fs = require("fs");
-const axios = require("axios");
 const _ = require("lodash");
-const cheerio = require("cheerio");
 const express = require("express");
 const nsfwMedia = require("./routes/media");
 const searchRoute = require("./routes/search");
@@ -12,7 +10,8 @@ app.get('/media/:value', (request, response) =>{
     nsfwMedia(request, fs, response, _);
 });
 app.get("/search", async(request, response) => {
-    searchRoute(request, response)
+    const replaceSearch = `${request.query.q}`?.replace(/ /g, "_")
+    searchRoute(replaceSearch, response)
 })
 app.get("/", async(request, response) => {
     response.send({"docs": "https://github.com/Swag666baby/nsfw-api"})
