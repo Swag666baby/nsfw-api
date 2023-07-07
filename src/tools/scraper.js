553cff64a7f7e3dab336e8cd8caf4b5a57091b44
Baby <89675173+Swag666baby@users.noreply.github.com>
2023-07-06 22:45:26 -0300

    Add files via upload
    fixing the search bug

--- src/tools/scraper.js
+++ src/tools/scraper.js
@@ -5,7 +5,7 @@ async function scraper(search){
     headers = {
         "User-Agent": "Mozilla/5.0 (Linux; Android 12; SM-S906N Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, como Gecko) Versão/4.0 Chrome/80.0.3987.119 Mobile Safari/537.36",
     }
-    const response = await axios.get(`https://www.rule34.us/index.php?r=posts/index&q=${search}`.replace(/%20/g, "_"), {headers});
+    const response = await axios.get(`https://www.rule34.us/index.php?r=posts/index&q=${search}`, {headers});
     let $ = cheerio.load(response.data);
     let data = []
     const images = $(".thumbail-container img").map((index, element) => {
