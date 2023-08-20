db26005b36e4c4346b527cb26e434c6599e72d78
Baby <89675173+Swag666baby@users.noreply.github.com>
2023-08-20 18:01:30 -0300

    Update search.ts

--- src/routes/hentai/search.ts
+++ src/routes/hentai/search.ts
@@ -2,7 +2,7 @@ import {surveyController} from "../../controllers/search_controller";
 import {Router, requests, response} from "express";
 const routes = Router();
 
-routes.get("/media/h/search", async(request, response) => {
+routes.get("/h/search", async(request, response) => {
 	const resp = await surveyController(request.query.q)
 	response.send(resp);
 });
